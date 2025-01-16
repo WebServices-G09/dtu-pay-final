d7b15b831daa85f13872155dd17d9c9cebd3f90d
Ionut Eusebiu Andrici <ionut.andrici2@mail.dcu.ie>
2025-01-16 16:56:02 +0100

    add payment repository methods

--- PaymentManagement/src/main/java/dtu/dtuPay/repositeries/PaymentRepository.java
+++ PaymentManagement/src/main/java/dtu/dtuPay/repositeries/PaymentRepository.java
@@ -2,12 +2,18 @@ package dtu.dtuPay.repositeries;
 
 import dtu.dtuPay.models.Payment;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 
 public class PaymentRepository {
     private static PaymentRepository instance;
     private HashMap<UUID, Payment> payments;
+    // <UUID paymentId, UUID customerId>
+    private HashMap<UUID, List<UUID>> customerPayments;
+    // <UUID paymentId, UUID merchantId>
+    private HashMap<UUID, List<UUID>> merchantPayments;
 
     private PaymentRepository() {
         this.payments = new HashMap<>();
@@ -22,5 +28,32 @@ public class PaymentRepository {
             }
         }
         return instance;
+    }
+
+    public void addPayment(Payment payment) {
+        this.payments.put(payment.getId(), payment);
+    }
+
+    public List<Payment> getPayments() {
+        List<Payment> paymentList = new ArrayList<>(this.payments.values());
+        return paymentList;
+    }
+
+    public List<Payment> getCustomerPayments(UUID customerId) {
+        List<UUID> customerPaymnetsIdList = customerPayments.get(customerId);
+        List<Payment> paymentList = getPayments().stream().filter(
+                payment -> customerPaymnetsIdList.contains(payment.getId())
+        ).toList();
+
+        return paymentList;
+    }
+
+    public List<Payment> getMerchantPayments(UUID merchantId) {
+        List<UUID> merchantPaymnetsIdList = merchantPayments.get(merchantId);
+        List<Payment> paymentList = getPayments().stream().filter(
+                payment -> merchantPaymnetsIdList.contains(payment.getId())
+        ).toList();
+
+        return paymentList;
     }
 }
--- PaymentManagement/src/main/java/dtu/dtuPay/services/PaymentService.java
+++ PaymentManagement/src/main/java/dtu/dtuPay/services/PaymentService.java
@@ -1,37 +1,66 @@
 package dtu.dtuPay.services;
 
+import dtu.dtuPay.models.Payment;
 import dtu.dtuPay.repositeries.PaymentRepository;
 import messaging.Event;
 import messaging.MessageQueue;
 import messaging.implementations.RabbitMqQueue;
 
+import java.util.List;
+import java.util.UUID;
+
 public class PaymentService {
 
+    private static final String GET_PAYMENTS_REQUESTED = "GetPaymentsRequested";
+    private static final String PAYMENTS_FETCHED = "PaymentsFetched";
+    private static final String GET_CUSTOMER_PAYMENTS_REQUESTED = "GetCustomerPaymentsRequested";
+    private static final String CUSTOMER_PAYMENTS_FETCHED = "CustomerPaymentsFetched";
+    private static final String GET_MERCHANT_PAYMENTS_REQUESTED = "GetMerchantPaymentsRequested";
+    private static final String MERCHANT_PAYMENTS_FETCHED = "MerchantPaymentsFetched";
+    private static final String PAYMENT_REQUESTED = "PaymentRequested";
+    private static final String PAYMENT_COMPLETED = "PaymentCompleted";
     MessageQueue queue;
     PaymentRepository paymentRepository = PaymentRepository.getInstance();
 
     public PaymentService(RabbitMqQueue mq) {
         this.queue = mq;
         // Get payments
-        this.queue.addHandler("GetPaymentsRequested", this::handleGetPaymentsRequested);
+        this.queue.addHandler(GET_PAYMENTS_REQUESTED, this::handleGetPaymentsRequested);
         // Get customerPayments
-        this.queue.addHandler("GetCustomerPaymentsRequested", this::handleGetCustomerPaymentsRequested);
+        this.queue.addHandler(GET_CUSTOMER_PAYMENTS_REQUESTED, this::handleGetCustomerPaymentsRequested);
         // Get merchantPayments
-        this.queue.addHandler("GetMerchantPaymentsRequested", this::handleGetMerchantPaymentsRequested);
+        this.queue.addHandler(GET_MERCHANT_PAYMENTS_REQUESTED, this::handleGetMerchantPaymentsRequested);
         // Request payment
-        this.queue.addHandler("PaymentRequested", this::handlePaymentRequested);
+        this.queue.addHandler(PAYMENT_REQUESTED, this::handlePaymentRequested);
     }
 
-    private void handlePaymentRequested(Event event) {
+    public void handlePaymentRequested(Event ev) {
+
+        Event event = new Event(PAYMENT_COMPLETED, new Object[] { });
+        queue.publish(event);
     }
 
-    private void handleGetMerchantPaymentsRequested(Event event) {
+    public void handleGetMerchantPaymentsRequested(Event ev) {
+        UUID merchantId = ev.getArgument(0, UUID.class);
+        List<Payment> paymentList = paymentRepository.getMerchantPayments(merchantId);
+
+        Event event = new Event(MERCHANT_PAYMENTS_FETCHED, new Object[] {paymentList});
+        queue.publish(event);
     }
 
-    private void handleGetCustomerPaymentsRequested(Event event) {
+    public void handleGetCustomerPaymentsRequested(Event ev) {
+        UUID customerId = ev.getArgument(0, UUID.class);
+        List<Payment> paymentList = paymentRepository.getCustomerPayments(customerId);
+
+        Event event = new Event(CUSTOMER_PAYMENTS_FETCHED, new Object[] {paymentList});
+        queue.publish(event);
     }
 
-    private void handleGetPaymentsRequested(Event event) {
+    public void handleGetPaymentsRequested(Event ev) {
+        List<Payment> paymentList = paymentRepository.getPayments();
+
+        Event event = new Event(PAYMENTS_FETCHED, new Object[] {paymentList});
+        queue.publish(event);
     }
 
 }
