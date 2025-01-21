7513651639f09ca723e2a0da58cf6f53d8056347
Ionut Eusebiu Andrici <ionut.andrici2@mail.dcu.ie>
2025-01-21 16:59:23 +0100

    end to end exception handling

--- DTU-Pay-Server/src/main/java/services/TokenService.java
+++ DTU-Pay-Server/src/main/java/services/TokenService.java
@@ -4,6 +4,7 @@ import messaging.Event;
 import messaging.MessageQueue;
 import messaging.implementations.RabbitMqQueue;
 import models.CorrelationId;
+import models.TokenEventMessage;
 
 import java.util.List;
 import java.util.Map;
@@ -20,8 +21,7 @@ public class TokenService {
     static TokenService service = null;
 
     private MessageQueue queue;
-    private Map<CorrelationId, CompletableFuture<Integer>> createTokensCorrelations = new ConcurrentHashMap<>();
-    private Map<CorrelationId, CompletableFuture<UUID>> requestTokensCorrelations = new ConcurrentHashMap<>();
+    private Map<CorrelationId, CompletableFuture<TokenEventMessage>> correlations = new ConcurrentHashMap<>();
 
     public static synchronized TokenService getInstance() {
         if (service != null) {
@@ -43,13 +43,13 @@ public class TokenService {
         CorrelationId correlationId = e.getArgument(0, CorrelationId.class);
         UUID token = e.getArgument(1, UUID.class);
 
-        requestTokensCorrelations.get(correlationId).complete(token);
+        correlations.get(correlationId).complete(token);
     }
 
     public UUID getToken(UUID customerId) {
         CorrelationId correlationId = CorrelationId.randomId();
         CompletableFuture<UUID> futureGetTokenCompleted = new CompletableFuture<>();
-        requestTokensCorrelations.put(correlationId, futureGetTokenCompleted);
+        correlations.put(correlationId, futureGetTokenCompleted);
 
         Event event = new Event(CUSTOMER_TOKENS_REQUESTED, new Object[] { correlationId, customerId });
         queue.publish(event);
@@ -61,13 +61,13 @@ public class TokenService {
         CorrelationId correlationId = e.getArgument(0, CorrelationId.class);
         Integer nTokensCreated = e.getArgument(1, Integer.class);
 
-        createTokensCorrelations.get(correlationId).complete(nTokensCreated);
+        correlations.get(correlationId).complete(nTokensCreated);
     }
 
     public int createTokens(UUID customerId, int nTokens) {
         CorrelationId correlationId = CorrelationId.randomId();
         CompletableFuture<Integer> futureCreateTokensCompleted = new CompletableFuture<>();
-        createTokensCorrelations.put(correlationId, futureCreateTokensCompleted);
+        correlations.put(correlationId, futureCreateTokensCompleted);
 
         Event event = new Event(REQUEST_TOKENS_EVENT, new Object[] { correlationId, customerId, nTokens });
         queue.publish(event);
